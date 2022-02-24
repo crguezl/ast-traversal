f42b0f36e9d29abc90c1a6f8fee55318edd74502
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-02-24 12:24:49 +0000

    babel toolset

---
+++ rnbyx.js
@@ -0,0 +1,30 @@
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import generate from "@babel/generator";
+
+const code = `function square(n) { /* a comment */
+  return n * n;
+}`;
+
+const ast = parser.parse(code);
+
+traverse(ast, {
+  enter(path) {
+    if (path.isIdentifier({ name: "n" })) {
+      path.node.name = "x";
+    }
+  },
+});
+
+//console.log(JSON.stringify(ast,null,2));
+
+const output = generate(
+    ast,
+    {
+      /* options */
+      comments: false
+    },
+    code
+);
+
+console.log(output);
